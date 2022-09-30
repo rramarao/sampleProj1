fba8d1b6d0391285f35048ff41023dab492d2fb6
rramarao <ramarao.ravipati@gmail.com>
2022-09-30 12:45:43 +0100

    commit msg

--- firstTest.js
+++ firstTest.js
@@ -1,5 +1,30 @@
-console.log("one");
-console.log("three");
-console.log("four");
-console.log("feature1");
-console.log("feature1 next");
+const arr = [200, 500, 400, -400, 300, 200, 5000, 250];
+
+// const sum = arr.reduce((acc, current, i, aray) => {
+//     console.log("i : " + i);
+
+//     if (acc < current) {
+//         console.log("acc : " + acc);
+//         return acc;
+//     } else {
+//         console.log("current : "+current);
+//         return current
+//     }
+    
+// },0)
+
+// console.log(sum)
+
+// const sortedArray = arr.sort(function (a, b) {
+//     // console.log("a value "+a);
+//         console.log("Array values "+arr);
+//     if (a - b > 0) {
+//         console.log(`positive-${a-b} and a value ${a} and b value ${b}`)
+//     } else {
+//         console.log(`negative-${a-b} and a value ${a} and b value ${b}`)
+//     }
+//     return a-b;
+// })
+// console.log(sortedArray);
+
+
